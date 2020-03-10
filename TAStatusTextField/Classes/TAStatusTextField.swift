5d6a6d3950cc9b41a5353caf963aaeb13587d692
tunay alver <tunay.alver@mobillium.com>
2020-03-10 18:40:08 +0300

    some cycles updated

--- TAStatusTextField/Classes/TAStatusTextField.swift
+++ TAStatusTextField/Classes/TAStatusTextField.swift
@@ -7,16 +7,14 @@
 
 import UIKit
 
-import UIKit
-
 @IBDesignable
 public class TAStatusTextField: UITextField {
         
     //MARK: - Outlets
     private var titleLabel: UILabel!
     private var bottomLine: UIView!
-    private var rightImageView: UIImageView!
     private var errorLabel: UILabel!
+    private var rightImageView: UIImageView!
     
     //MARK: - Image Names
     @IBInspectable var normalImage: UIImage?
@@ -32,9 +30,16 @@ public class TAStatusTextField: UITextField {
     var rightImageViewRightPadding = CGFloat(20)
     
     //MARK: - Properties
-    @IBInspectable var title: String!
-    
-    @IBInspectable var errorText: String!
+    @IBInspectable var titleText: String? {
+        didSet {
+            titleLabel.text = titleText
+        }
+    }
+    @IBInspectable var errorText: String? {
+        didSet {
+            errorLabel.text = errorText
+        }
+    }
     
     var shouldSecureText: Bool?
     
@@ -53,92 +58,99 @@ public class TAStatusTextField: UITextField {
     }
     
     //MARK: - Colors
-    @IBInspectable var fieldTextColor: UIColor! = .clear {
-        didSet {
-            self.updateText(withColor: fieldTextColor)
-        }
-    }
-    @IBInspectable var fieldErrorLabelColor: UIColor! = .clear {
-        didSet {
-            self.updateErrorLabel(isHidden: true)
-        }
-    }
+    @IBInspectable var fieldTextColor: UIColor! = .black
+    
+//    @IBInspectable var normalColor: UIColor?
+//    @IBInspectable var editingColor: UIColor?
+//    @IBInspectable var successColor: UIColor?
+//    @IBInspectable var errorColor: UIColor?
+    
+    @IBInspectable var errorLabelColor: UIColor! = .black
     
     //MARK: - Colors Normal
-    @IBInspectable var fieldNormalTitleColor: UIColor! = .clear {
-        didSet {
-            self.updateTitleLabel(withColor: fieldNormalTitleColor)
-        }
-    }
-    @IBInspectable var fieldNormalBackgroundColor: UIColor! = .clear {
-        didSet {
-            self.backgroundColor = fieldNormalBackgroundColor
-        }
-    }
-    @IBInspectable var fieldNormalBottomLineColor: UIColor! = .clear {
-        didSet {
-            self.updateBottomLine(withColor: fieldNormalBottomLineColor)
-        }
-    }
+    @IBInspectable var normalTitleColor: UIColor! = .clear
+    @IBInspectable var normalBackgroundColor: UIColor! = .clear
+    @IBInspectable var normalBottomLineColor: UIColor! = .clear
     
     //MARK: - Colors Editing
-    @IBInspectable var fieldEditingTitleColor: UIColor! = .clear
-    @IBInspectable var fieldEditingBackgroundColor: UIColor! = .clear
-    @IBInspectable var fieldEditingBottomLineColor: UIColor! = .clear
+    @IBInspectable var editingTitleColor: UIColor! = .clear
+    @IBInspectable var editingBackgroundColor: UIColor! = .clear
+    @IBInspectable var editingBottomLineColor: UIColor! = .clear
     
     //MARK: - Colors Success
-    @IBInspectable var fieldSuccessTitleColor: UIColor! = .clear
-    @IBInspectable var fieldSuccessBackgroundColor: UIColor! = .clear
-    @IBInspectable var fieldSuccessBottomLineColor: UIColor! = .clear
+    @IBInspectable var successTitleColor: UIColor! = .clear
+    @IBInspectable var successBackgroundColor: UIColor! = .clear
+    @IBInspectable var successBottomLineColor: UIColor! = .clear
     
     //MARK: - Colors Error
-    @IBInspectable var fieldErrorTitleColor: UIColor! = .clear
-    @IBInspectable var fieldErrorBackgroundColor: UIColor! = .clear
-    @IBInspectable var fieldErrorBottomLineColor: UIColor! = .clear
+    @IBInspectable var errorTitleColor: UIColor! = .clear
+    @IBInspectable var errorBackgroundColor: UIColor! = .clear
+    @IBInspectable var errorBottomLineColor: UIColor! = .clear
     
     //MARK: - Init Functions
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
         commonInit()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        
         commonInit()
     }
     
     func commonInit() {
-        addViews()
-        updateViews()
+        self.addTitleLabel()
+        self.addErrorLabel()
+        self.addBottomLine()
     }
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        updateSubviewsFrame()
+    }
+    
+   
     
     //MARK: - Setup
     public func setupFieldTexts(titleText: String?, errorText: String?, placeholderText: String?) {
-        self.title = titleText
+        self.titleText = titleText
         self.errorText = errorText
         self.placeholder = placeholderText
+        
+//        self.addTitleLabel()
+//        self.addErrorLabel()
     }
     
-    public func setupFieldColors(fieldNormalTitleColor: UIColor?, fieldNormalBackgroundColor: UIColor?, fieldNormalBottomLineColor: UIColor?,
+    public func setupFieldColors(fieldNormalTitleColor: UIColor?, fieldNormalBackgroundColor: UIColor?, normalBottomLineColor: UIColor?,
                      fieldEditingTitleColor: UIColor?, fieldEditingBackgroundColor: UIColor?, fieldEditingBottomLineColor: UIColor?,
                      fieldSuccessTitleColor: UIColor?, fieldSuccessBackgroundColor: UIColor?, fieldSuccessBottonLineColor: UIColor?,
                      fieldErrorTitleColor: UIColor?, fieldErrorBackgroundColor: UIColor?, fieldErrorBottomLineColor: UIColor?) {
        
-        self.fieldNormalTitleColor = fieldNormalTitleColor
-        self.fieldNormalBackgroundColor = fieldNormalBackgroundColor
-        self.fieldNormalBottomLineColor = fieldNormalBottomLineColor
+        self.normalTitleColor = fieldNormalTitleColor
+        self.normalBackgroundColor = fieldNormalBackgroundColor
+        self.normalBottomLineColor = normalBottomLineColor
         
-        self.fieldEditingTitleColor = fieldEditingTitleColor
-        self.fieldEditingTitleColor = fieldEditingTitleColor
-        self.fieldEditingBottomLineColor = fieldEditingBottomLineColor
+        self.editingTitleColor = fieldEditingTitleColor
+        self.editingTitleColor = fieldEditingTitleColor
+        self.editingBottomLineColor = fieldEditingBottomLineColor
         
-        self.fieldSuccessTitleColor = fieldSuccessTitleColor
-        self.fieldSuccessBackgroundColor = fieldSuccessBackgroundColor
-        self.fieldSuccessBottomLineColor = fieldSuccessBottonLineColor
+        self.successTitleColor = fieldSuccessTitleColor
+        self.successBackgroundColor = fieldSuccessBackgroundColor
+        self.successBottomLineColor = fieldSuccessBottonLineColor
         
-        self.fieldErrorTitleColor = fieldErrorTitleColor
-        self.fieldErrorBackgroundColor = fieldErrorBackgroundColor
-        self.fieldErrorBottomLineColor = fieldErrorBottomLineColor
+        self.errorTitleColor = fieldErrorTitleColor
+        self.errorBackgroundColor = fieldErrorBackgroundColor
+        self.errorBottomLineColor = fieldErrorBottomLineColor
+        
+//        self.addBottomLine()
+        self.updateText(withColor: fieldTextColor)
     }
     
     public func setupRightImages(normalImage: UIImage?, editingImage: UIImage?, successImage: UIImage?, errorImage: UIImage?) {
@@ -148,43 +160,32 @@ public class TAStatusTextField: UITextField {
         self.errorImage = errorImage
     }
     
-    override public func draw(_ rect: CGRect) {
-        updateSubviewsFrame()
-    }
-    
     //MARK: - Update Functions
-    public func addViews() {
-        self.addTitleLabel()
-        self.updateText(withColor: fieldTextColor)
-        self.addErrorLabel()
-        self.addBottomLine()
-    }
-    
-    func updateViews() {
+    public func updateViews() {
         self.isSecureTextEntry = shouldSecureText ?? false
         switch status {
         case .normal:
-            self.backgroundColor = fieldNormalBackgroundColor
-            self.updateTitleLabel(withColor: fieldNormalTitleColor)
-            self.updateBottomLine(withColor: fieldNormalBottomLineColor)
+            self.backgroundColor = normalBackgroundColor
+            self.updateTitleLabel(withColor: normalTitleColor)
+            self.updateBottomLine(withColor: normalBottomLineColor)
             self.updateRightImage(image: normalImage)
             self.updateErrorLabel(isHidden: true)
         case .editing:
-            self.backgroundColor = fieldEditingBackgroundColor
-            self.updateTitleLabel(withColor: fieldEditingTitleColor)
-            self.updateBottomLine(withColor: fieldEditingBottomLineColor)
+            self.backgroundColor = editingBackgroundColor
+            self.updateTitleLabel(withColor: editingTitleColor)
+            self.updateBottomLine(withColor: editingBottomLineColor)
             self.updateRightImage(image: editingImage)
             self.updateErrorLabel(isHidden: true)
         case .success:
-            self.backgroundColor = fieldSuccessBackgroundColor
-            self.updateTitleLabel(withColor: fieldSuccessTitleColor)
+            self.backgroundColor = successBackgroundColor
+            self.updateTitleLabel(withColor: successTitleColor)
             self.updateRightImage(image: successImage)
-            self.updateBottomLine(withColor: fieldSuccessBottomLineColor)
+            self.updateBottomLine(withColor: successBottomLineColor)
             self.updateErrorLabel(isHidden: true)
         case .error:
-            self.backgroundColor = fieldErrorBackgroundColor
-            self.updateTitleLabel(withColor: fieldErrorTitleColor)
-            self.updateBottomLine(withColor: fieldErrorBottomLineColor)
+            self.backgroundColor = errorBackgroundColor
+            self.updateTitleLabel(withColor: errorTitleColor)
+            self.updateBottomLine(withColor: errorBottomLineColor)
             self.updateRightImage(image: errorImage)
             self.updateErrorLabel(isHidden: false)
         }
@@ -193,15 +194,16 @@ public class TAStatusTextField: UITextField {
     //MARK: - Title
     func addTitleLabel() {
         titleLabel = UILabel(frame: CGRect(x: defaultPadding, y: titleTopPadding, width: self.frame.width, height: 20))
-        titleLabel.textColor = fieldNormalTitleColor
+        titleLabel.textColor = normalTitleColor
         titleLabel.translatesAutoresizingMaskIntoConstraints = true
         titleLabel.textAlignment = .left
         titleLabel.numberOfLines = 1
+        titleLabel.text = titleText
         self.addSubview(titleLabel)
     }
     
     func updateTitleLabel(withColor: UIColor) {
-        guard titleLabel != nil else { return }
+//        guard titleLabel != nil else { return }
         titleLabel.textColor = withColor
     }
     
@@ -229,33 +231,31 @@ public class TAStatusTextField: UITextField {
     //MARK: - Error Label
     func addErrorLabel() {
         errorLabel = UILabel(frame: CGRect(x: defaultPadding, y: self.frame.height - 20, width: self.frame.width, height: 20))
-        errorLabel.textColor = fieldErrorLabelColor
-        errorLabel.translatesAutoresizingMaskIntoConstraints = true
-        errorLabel.textAlignment = .left
-        errorLabel.numberOfLines = 1
-        errorLabel.isHidden = true
-        self.addSubview(errorLabel)
+        errorLabel?.textColor = errorLabelColor
+        errorLabel?.translatesAutoresizingMaskIntoConstraints = true
+        errorLabel?.textAlignment = .left
+        errorLabel?.numberOfLines = 1
+        errorLabel?.isHidden = true
+        errorLabel.text = errorText
+        self.addSubview(errorLabel!)
     }
     
     func updateErrorLabel(isHidden: Bool) {
-        guard errorLabel != nil else { return }
-        errorLabel.isHidden = isHidden
+        errorLabel?.isHidden = isHidden
     }
     
     func addBottomLine() {
         bottomLine = UIView(frame: CGRect(x: 20, y: self.frame.height - bottomLineBottomPadding, width: self.frame.width - 2*defaultPadding, height: bottomLineHeight))
-        bottomLine.backgroundColor = fieldNormalBackgroundColor
-        self.addSubview(bottomLine)
+        bottomLine?.backgroundColor = normalBackgroundColor
+        self.addSubview(bottomLine!)
     }
     
     func updateBottomLine(withColor: UIColor) {
-        guard bottomLine != nil else { return }
-         bottomLine.backgroundColor = withColor
+        bottomLine?.backgroundColor = withColor
     }
     
     func updateSubviewsFrame() {
-        guard bottomLine != nil else { return }
-        bottomLine.frame = CGRect(x: 20, y: self.frame.height - bottomLineBottomPadding, width: self.frame.width - 2*defaultPadding, height: bottomLineHeight)
+        bottomLine?.frame = CGRect(x: 20, y: self.frame.height - bottomLineBottomPadding, width: self.frame.width - 2*defaultPadding, height: bottomLineHeight)
     }
     
     //MARK: - Text Rect
@@ -266,6 +266,8 @@ public class TAStatusTextField: UITextField {
     override public func editingRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: UIEdgeInsets(top: defaultPadding, left: defaultPadding, bottom: 0, right: defaultPadding + 16))
     }
+    
+    
     
     //MARK: - Right View Rect
     override public func rightViewRect(forBounds bounds: CGRect) -> CGRect {
